9c7a542b23ce06104a54fe572335cc1d4ce435c5
Kavya-dot-cloud <kaavya.vasant@gmail.com>
2024-10-23 11:55:01 +0000

    Added Jinja Template

--- app.py
+++ app.py
@@ -2,8 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS=[
+{
+    'id':1,
+    'title':'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary':'Rs. 10,00,000'
+},
+{   'id':2,
+    'title':'UI Developer',
+    'location': 'Delhi, India',
+    'salary':'Rs. 12,00,000' 
+        
+},
+{
+    'id':3,
+    'title':'Devops Engineer',
+    'location': 'Chennai, India',
+    'salary':'Rs. 11,00,000'
+},
+{
+    'id':4,
+    'title':'Data Engineer',
+    'location': 'Bengaluru, India',
+    #'salary':'Rs. 15,00,000'
+}
+    
+]
+
 @app.route("/")
 def hello_kavya():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug=True)
